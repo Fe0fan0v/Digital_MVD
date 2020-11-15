a67471112efe524871977f14b8ce77d6bc490584
vadim <feofanov@yandexlyceum.ru>
2020-11-15 19:02:58 +0500

    3rd person try

--- test.py
+++ test.py
@@ -1,7 +1,13 @@
+from nltk.tokenize import word_tokenize
 import pymorphy2
-morph = pymorphy2.MorphAnalyzer()
 
+text = 'Я шла по шоссе'
 
-from PyDictionary import PyDictionary
-dictionary = PyDictionary()
-print(dictionary.synonym("хорошо"))
+tokenized = word_tokenize(text)
+morph = pymorphy2.MorphAnalyzer(lang='ru')
+for i in range(len(tokenized)):
+    if morph.parse(tokenized[i])[0].tag.person == '1per':
+        print(morph.parse(tokenized[i])[0].inflect({'3per'}))
+        tokenized[i] = morph.parse(tokenized[i])[0].inflect({'3per'})
+
+print(' '.join(tokenized))
