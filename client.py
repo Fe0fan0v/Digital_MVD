c889b2adea5885cc3385607a0b51347876aee1cc
vadim <feofanov@yandexlyceum.ru>
2020-11-17 19:46:59 +0500

    Add datebase

--- client.py
+++ client.py
@@ -2,14 +2,14 @@ import socket
 import json
 
 ID = 1
-client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # соединяем
 client_sock.connect(('127.0.0.1', 53210))
 
 with open('txt', 'r') as file:
-    text = ' '.join(file.readlines())
+    text = ' '.join(file.readlines())  # читаем из файла
 
-client_sock.sendall(json.dumps({'id': ID, 'text': text}).encode('utf-8'))
-response = client_sock.recv(2048)
+client_sock.sendall(json.dumps({'id': ID, 'text': text}).encode('utf-8'))  # отправляем на сервер данные
+response = client_sock.recv(2048)  # получаем ответ от сервера
 client_sock.close()
 
 print('Received', json.loads(response)['text'])
--- server.py
+++ server.py
@@ -1,20 +1,34 @@
 from text import Text
 import socket
 import json
+import sqlite3
+from datetime import datetime
 
 serv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
 serv_sock.bind(('', 53210))
 serv_sock.listen(10)
+
 
 while True:
     client_sock, client_addr = serv_sock.accept()
     print('Connected by', client_addr)
 
     while True:
-        request = json.loads(client_sock.recv(2048))
-        text_to_work = Text(request['id'], request['text'])
-        if not text_to_work:
+        request = json.loads(client_sock.recv(2048))  # получаем запрос
+        text_to_work = Text(request['id'], request['text'])  # обрабатываем
+        if not text_to_work:  # если нет запроса отключаем
             break
-        client_sock.sendall((json.dumps({'text': text_to_work.change_person()}).encode('utf-8')))
+        time = f'{datetime.now().hour}:{datetime.now().minute}:{datetime.now().second}'  # собираем и обраб. данные
+        date = f'{datetime.now().day}/{datetime.now().month}/{datetime.now().year}'
+        text = text_to_work.change_person()
+
+        connection = sqlite3.connect("mvd.sqlite")  # записываем в БД
+        cursor = connection.cursor()
+        cursor.execute(f"""INSERT INTO documents
+                          VALUES (?, ?, ?, ?);""", (int(text_to_work.id), date, time, text))
+        connection.commit()
+        connection.close()
+
+        client_sock.sendall((json.dumps({'text': text}).encode('utf-8')))  # отправляем ответ клиенту
 
     client_sock.close()
